09f50b2368bb3c34ee04317e1d68c52fe07e9175
radusancraian <radu.sancraian@freshbyteinc.com>
2016-07-30 21:44:01 +0300

    Basic Table and inclusion of libraries

--- index.php
+++ index.php
@@ -1,4 +1,66 @@
-<?PHP
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Atom.Tracker</title>
 
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!--  FontAwesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-ECHO 'hELLO WORLD right now"';
+</head>
+
+<body>
+<div class="container-fluid">
+    <header class="row">
+
+        <div class="col-xs-10">
+            Input here
+        </div>
+
+        <div class="col-xs-2">
+            Button here
+        </div>
+
+        <hr>
+        <table class="table table-bordered">
+            <thead>
+
+            <tr>
+                <th>Task</th>
+                <th>Start</th>
+                <th>End</th>
+                <th>Time</th>
+                <th colspan="2">Controls</th>
+
+            </tr>
+
+            </thead>
+             <tbody>
+               <tr>
+                   <td>Data</td>
+                   <td>Data</td>
+                   <td>Data</td>
+                   <td>Data</td>
+                   <td>Button</td>
+                   <td>Button</td>
+
+               </tr>
+             </tbody>
+
+        </table>
+    </header>
+</div>
+
+<!-- JQuery -->
+<script src="https://code.jquery.com/jquery-3.1.0.js" integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="
+        crossorigin="anonymous"></script
+
+    <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>
