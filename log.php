151cee4a51906b0bcd5a7a8363564d5d119b3bdc
radusancraian <radu.sancraian@freshbyteinc.com>
2016-08-22 23:37:26 +0300

    Add date to Json

--- log.php
+++ log.php
@@ -9,8 +9,8 @@ foreach($data as $task)
 
     <tr>
         <td><?= $task['name'] ?></td>
-        <td>Data</td>
-        <td>Data</td>
+        <td><?php echo date_nice($task['date_start']) ?></td>
+        <td><?php echo date_nice($task['date_end']) ?></td>
         <td>Data</td>
         <td><button class="btn btn-primary"><?php echo i('stop'); ?></button></td>
         <td><button class="btn btn-danger"><?php echo i('times'); ?></button></td>
